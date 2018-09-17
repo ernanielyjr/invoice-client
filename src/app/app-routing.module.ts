bc454b57c1b46fcb3a929f285a710bc6718b9da1
Ernani Ely Jr <ernani.ely@softdesign-rs.com.br>
2018-09-17 15:30:46 -0300

    Base components: invoice and forbidden

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { ForbiddenComponent } from './pages/forbidden/forbidden.component';
+import { InvoiceComponent } from './pages/invoice/invoice.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: 'invoice/:invoiceId',
+  component: InvoiceComponent
+}, {
+  path: '',
+  component: ForbiddenComponent,
+}, {
+  path: '**',
+  redirectTo: '/',
+  pathMatch: 'prefix',
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InvoiceComponent } from './pages/invoice/invoice.component';
+import { ForbiddenComponent } from './pages/forbidden/forbidden.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InvoiceComponent,
+    ForbiddenComponent
   ],
   imports: [
     BrowserModule,
