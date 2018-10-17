8526304831b11c590e5489861b46b7d9bae1c582
Ernani Ely Jr <ernani.ely@softdesign-rs.com.br>
2018-10-17 11:17:06 -0300

    Node serve file - fix path app

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/myplan-client'));
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname+'/dist/index.html'));
+  res.sendFile(path.join(__dirname+'/dist/myplan-client/index.html'));
 });
 app.listen(process.env.PORT || 8080);
