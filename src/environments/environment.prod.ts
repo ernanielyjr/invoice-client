423222c3773b8146ccc2b3324163133ac4b906a5
Ernani Ely Jr <ernani.ely@softdesign-rs.com.br>
2018-10-17 14:23:17 -0300

    Fix url of service

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ import environmentModel from './environment.model';
 
 export const environment: environmentModel = {
   production: true,
-  baseUrl: 'https://gautihost.herokuapp.com/api'
+  baseUrl: 'https://gauti-invoice-api.herokuapp.com//api'
 };
